0df7a81f6f1a90d6d5b132f0b415afc05be15acb
Elder <elderfavsto@gmail.com>
2023-03-04 20:38:46 -0300

    mysql

--- Template Engine/9_css_handlebars/index.js
+++ Template Engine/9_css_handlebars/index.js
@@ -4,55 +4,55 @@ const exphbs = require("express-handlebars");
 const app = express();
 
 const hbs = exphbs.create({
-  partialsDir: ['views/partials']
-})
+  partialsDir: ["views/partials"],
+});
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/dashboard', (req, res) => {
-  const items = ['item a', 'item b', 'item c']
+app.get("/dashboard", (req, res) => {
+  const items = ["item a", "item b", "item c"];
 
-  res.render('dashboard', {items})
-})
+  res.render("dashboard", { items });
+});
 
-app.get('/post', (req, res) => {
+app.get("/post", (req, res) => {
   const post = {
-    title: 'aprender node.js',
-    category: 'javascript',
-    body: 'este artigo vai te ajudar a aprender nodejs',
-    comments: 4
-  }
+    title: "aprender node.js",
+    category: "javascript",
+    body: "este artigo vai te ajudar a aprender nodejs",
+    comments: 4,
+  };
 
-  res.render('blogpost', {post})
-})
+  res.render("blogpost", { post });
+});
 
-app.get('/blog', (req, res) => {
+app.get("/blog", (req, res) => {
   const posts = [
     {
-      title: 'aprender a cozinhar',
-      category: 'cozinhar',
-      body: 'figado acebolado',
-      comments: 10
+      title: "aprender a cozinhar",
+      category: "cozinhar",
+      body: "figado acebolado",
+      comments: 10,
     },
     {
-      title: 'aprender a costurar',
-      category: 'costurar',
-      body: 'fazer vestido',
-      comments: 30
+      title: "aprender a costurar",
+      category: "costurar",
+      body: "fazer vestido",
+      comments: 30,
     },
     {
-      title: 'aprender mecanica',
-      category: 'macanica',
-      body: 'criar um motor',
-      comments: 80
-    }
-  ]
+      title: "aprender mecanica",
+      category: "macanica",
+      body: "criar um motor",
+      comments: 80,
+    },
+  ];
 
-  res.render('blog', {posts})
-})
+  res.render("blog", { posts });
+});
 
 app.get("/", (req, res) => {
   const user = {
@@ -61,12 +61,12 @@ app.get("/", (req, res) => {
   };
 
   const color = {
-    cor: 'branca',
-    cabelo: 'curto'
-  }
+    cor: "branca",
+    cabelo: "curto",
+  };
 
-  const auth = false
-  const approved = false
+  const auth = false;
+  const approved = false;
 
   res.render("home", { user: user, color, auth, approved });
 });
